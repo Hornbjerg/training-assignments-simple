9a68adfc2bf9720105300d4da4d1b12da247ae46
René Kjær Hornbjerg <r.hornbjerg@gmail.com>
2020-01-29 13:11:33 +0100

    Simplyfi getting colors for nationality
    Signed-off-by: René Kjær Hornbjerg <r.hornbjerg@gmail.com>

--- src/java/eu/sig/training/ch03/Nationality.java
+++ src/java/eu/sig/training/ch03/Nationality.java
@@ -1,5 +1,32 @@
 package eu.sig.training.ch03;
 
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
 public enum Nationality {
-    DUTCH, GERMAN, BELGIAN, LUXEMBOURGER, FRENCH, ITALIAN, ROMANIA, IRELAND, HUNGARIAN, BULGARIAN, RUSSIA, UNCLASSIFIED
+    DUTCH (Arrays.asList(Color.RED, Color.WHITE, Color.BLUE)),
+    GERMAN (Arrays.asList(Color.BLACK, Color.RED, Color.YELLOW)),
+    BELGIAN (Arrays.asList(Color.BLACK, Color.YELLOW, Color.RED)),
+    LUXEMBOURGER (),
+    FRENCH (Arrays.asList(Color.BLUE, Color.WHITE, Color.RED)),
+    ITALIAN (Arrays.asList(Color.GREEN, Color.WHITE, Color.RED)),
+    ROMANIA (Arrays.asList(Color.BLUE, Color.YELLOW, Color.RED)),
+    IRELAND (Arrays.asList(Color.GREEN, Color.WHITE, Color.ORANGE)),
+    HUNGARIAN (Arrays.asList(Color.RED, Color.WHITE, Color.GREEN)),
+    BULGARIAN (Arrays.asList(Color.WHITE, Color.GREEN, Color.RED)),
+    RUSSIA (Arrays.asList(Color.WHITE, Color.BLUE, Color.RED)),
+    UNCLASSIFIED ();
+
+	private List<Color> colors = Arrays.asList(Color.GRAY);
+
+	private Nationality() {}
+
+    private Nationality(List<Color> colors) {
+    	this.colors = colors;
+    }
+
+    public List<Color> getColors() {
+    	return this.colors;
+    }
 }
