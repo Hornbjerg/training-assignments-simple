3d023341a8256da7957903931bb413b590d31df9
René Kjær Hornbjerg <r.hornbjerg@gmail.com>
2020-01-29 13:20:50 +0100

    Removed duplicate functionality in BinaryTreeSearch
    Signed-off-by: René Kjær Hornbjerg <r.hornbjerg@gmail.com>

--- src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
+++ src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
@@ -2,29 +2,22 @@ package eu.sig.training.ch03.binarytree;
 
 public class BinaryTreeSearch {
 
-    // tag::calculateDepth[]
     public static int calculateDepth(BinaryTreeNode<Integer> node, int nodeValue) {
         int depth = 0;
         if (node.getValue() == nodeValue) {
             return depth;
-        } else {
-            if (nodeValue < node.getValue()) {
-                BinaryTreeNode<Integer> left = node.getLeft();
-                if (left == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(left, nodeValue);
-                }
-            } else {
-                BinaryTreeNode<Integer> right = node.getRight();
-                if (right == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(right, nodeValue);
-                }
-            }
         }
+        if (nodeValue < node.getValue()) {
+        	return handleNode(node.getLeft(), nodeValue);
+        }
+        return handleNode(node.getRight(), nodeValue);
     }
-    // end::calculateDepth[]
 
+	private static int handleNode(BinaryTreeNode<Integer> node, int nodeValue) {
+		if (node == null) {
+			throw new TreeException("Value not found in tree!");
+		} else {
+			return 1 + calculateDepth(node, nodeValue);
+		}
+	}
 }
